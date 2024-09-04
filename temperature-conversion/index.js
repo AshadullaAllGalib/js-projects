340411c5541c528359c2d314bafe4a109b402344
“Ashadulla” <ashadullaalgalib@gmail.com>
2024-09-04 09:49:14 +0600

    updated some code

--- temperature-conversion/index.js
+++ temperature-conversion/index.js
@@ -10,11 +10,13 @@ function convert(){
         temp = Number(textBox.value);
         temp = temp * 9 / 5 +32;
         result.textContent = temp.toFixed(1) + "°F";
-    }else if(toCelsius.checked){
+    }
+    else if(toCelsius.checked){
         temp = Number(textBox.value);
         temp = (temp - 32) * (5 / 9);
         result.textContent = temp.toFixed(1) + "°C";
-    }else{
+    }
+    else{
         result.textContent = "Select a unit"
     }
 }
