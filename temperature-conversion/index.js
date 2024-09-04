78604676a09f52abe94c720527d37639960afb01
“Ashadulla” <ashadullaalgalib@gmail.com>
2024-09-04 10:16:28 +0600

    testing

--- temperature-conversion/index.js
+++ temperature-conversion/index.js
@@ -1,9 +1,6 @@
 const textBox = document.getElementById("textBox");
-
 const toFahrenheit = document.getElementById("toFahrenheit");
-
 const toCelsius = document.getElementById("toCelsius");
-
 const result = document.getElementById("result");
 let temp;
 
